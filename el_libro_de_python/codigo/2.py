850dd5157cd8071883966baf2f1556187c07f7ed
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-04-21 07:05:30 -0600

    21/04/2023 07:25 casa

---
+++ el_libro_de_python/codigo/2.py
@@ -0,0 +1,20 @@
+print("hola mundo")
+
+#creacion de variable
+x = 5
+print(x)
+
+#declaracion de variables a y b = creacion
+a = 3
+b = 7
+
+#uso de +
+print(a + b)
+
+#sentencia condicional
+a = 10
+if a == 10:
+    print("es 10")
+else:
+    print("no es 10")
+    
