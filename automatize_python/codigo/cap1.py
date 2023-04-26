21a5506563fd948e7ff8726619255535617acd99
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-04-25 22:02:52 -0600

    25/04/2023 22:00 casa

---
+++ automatize_python/codigo/cap1.py
@@ -0,0 +1,15 @@
+#operadores
+#.......suma
+2+2
+#.......exponente
+2**2
+#.......resto: devuelve el resto de una division
+10//3
+#.......division: devuelve el cociente
+22/8
+#.......multiplicacion
+3*5
+#.......sustraccion
+5-2
+
+
