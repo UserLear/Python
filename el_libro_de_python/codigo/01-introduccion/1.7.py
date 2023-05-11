aca5785148100c58aa0ede5588d9b48062bd9dfb
Moises Ayala <mayalam1991@gmail.com>
2023-05-11 16:17:11 -0600

    11/05/2023 16:17 oficina

--- el_libro_de_python/codigo/01-introduccion/1.7.py
+++ el_libro_de_python/codigo/01-introduccion/1.7.py
@@ -48,3 +48,55 @@ test.myfun2()
 #Resultado de salida:
 #a =  1
 #b =  2
+
+#.......alcance global
+# No se pueden cambiar enteros, cadenas, etc.
+a = 1  # a es una variable global
+
+def myfun():
+    a = 2  # Variables locales
+    a += 1
+    print("En la función a =", a)
+
+myfun()
+print("Fuera de la función a =", a)
+
+#Resultado de salida:
+#En la función a =  3
+#Fuera de la función a =  1
+
+# Listas, tuplas, etc. se pueden cambiar
+a = [1, 2, 3]
+b = {'Idioma': 98, "matemáticas": 101}
+
+def myfun():
+    a.append(4)
+    b.update({"Inglés": 103})
+    print("En la función a =", a)
+    print("En la función b =", b)
+
+
+myfun()
+print("Fuera de la función a =", a)
+print("Función externa b =", b)
+
+#Resultado de salida:
+#En la función a =  [1, 2, 3, 4]
+#En la función b =  {'Idioma': 98, 'matemáticas': 101, 'Inglés': 103}
+#Fuera de la función a =  [1, 2, 3, 4]
+#Función exterior b =  {'Idioma': 98, 'matemáticas': 101, 'Inglés': 103}
+
+# Use palabras clave globales para hacer referencia a variables globales
+a = 1
+
+def myfun():
+    global a  # Use global antes de usar un
+    a += 1
+    print("En la función a =", a)
+
+myfun()
+print("Fuera de la función a =", a)
+
+#Resultado de salida:
+#En la función a =  2
+#Fuera de la función a =  2
