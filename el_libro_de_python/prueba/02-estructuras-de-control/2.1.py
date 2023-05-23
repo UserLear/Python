e3f77fae53b107743b35fc9735b5bdb57ac1c16f
Moises Ayala <mayalam1991@gmail.com>
2023-05-23 16:37:50 -0600

    23/05/2023 16:37 oficina

--- el_libro_de_python/prueba/02-estructuras-de-control/2.1.py
+++ el_libro_de_python/prueba/02-estructuras-de-control/2.1.py
@@ -3,14 +3,16 @@
 #1. seleccionar en base a si es vegetariano, carniboro o omnivoro
 #2. tiempo de comida
 #3. ocacion del platillo
-#4. # de comenzales (niños y adultos)
+#4. tipos (niños y adultos) y # de comenzales (niños y adultos)
 #5. seleccion de vegetales o carnes disponibles para un platillo
 #6. nombre de platillos
 #7. tipos de cortes en vegetales y carnes (tiempo de preparacion)
 #8. receta (cantidades y mezclas, tiempo de preparacion)
 #9. generacion de presupuesto
-#10. acompañantes
+#10. generacion de menu semanal o mensual
+#11. acompañantes
 
+#1. seleccionar en base si es vegetariano, carniboro o omnivoro
 p = "v"
 if p == "vegetariano":
     print("no le cocines carne, no le gusta")
@@ -19,5 +21,4 @@ elif p == "carnivoro":
 else: 
     print("el come de todo, sirvele lo que sea")
 
-
-#usar for 
+ 
