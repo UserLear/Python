34996f1e0492d5e525cda85e45f08f666539f4b7
Moises Ayala <mayalam1991@gmail.com>
2023-05-29 16:21:55 -0600

    29/05/2023 16:21 oficina

--- automatize_python/codigo/cap1.py
+++ automatize_python/codigo/cap1.py
@@ -1,4 +1,4 @@
-#operadores
+#operadores matematicos
 #.......suma
 2+2
 #.......exponente
@@ -11,5 +11,18 @@
 3*5
 #.......sustraccion
 5-2
+
+#tipos de datos
+print(type(2))
+print(type(2.43))
+print(type("o", 'h'))
+
+#concatenacion y replicacion de cadenas
+x1 = "ho"
+print(x1 + "la") #concatenacion
+
+print(x1 * 5) #replicacion
+
+
 
 
