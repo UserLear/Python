c210aca3309bdf36600d187dd3e15eb858fe95e3
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-05-30 09:58:18 -0600

    30/05/2023 09:58 oficina

---
+++ python_para_todos/codigo/cap2.py
@@ -0,0 +1,15 @@
+#2.1 Valores y tipos . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 19
+#2.2 Variables . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 20
+#2.3 Nombres de variables y palabras claves . . . . . . . . . . . . . . . . 21
+#2.4 Sentencias . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 22
+#2.5 Operadores y operandos . . . . . . . . . . . . . . . . . . . . . . . . 22
+#2.6 Expresiones . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 23
+#2.7 Orden de las operaciones . . . . . . . . . . . . . . . . . . . . . . . 24
+#2.8 Operador módulo . . . . . . . . . . . . . . . . . . . . . . . . . . . 24
+#2.9 Operaciones con cadenas . . . . . . . . . . . . . . . . . . . . . . . 25
+#2.10 Petición de información al usuario . . . . . . . . . . . . . . . . . . 25
+#2.11 Comentarios . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 26
+#2.12 Elección de nombres de variables mnemónicos . . . . . . . . . . . . 27
+#2.13 Depuración . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 29
+#2.14 Glosario . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 29
+#2.15 Ejercicios . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 31
