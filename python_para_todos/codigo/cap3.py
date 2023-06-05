c7174684999b2b158303cff8f05ece118eaa2886
Moises Ayala <mayalam1991@gmail.com>
2023-06-05 16:58:59 -0600

    05/06/2023 16:58 oficina

---
+++ python_para_todos/codigo/cap3.py
@@ -0,0 +1,18 @@
+#EJECUCION CONDICIONAL
+#3.1.......expresiones booleanas
+x = 4
+y = 5
+print(x == y) #False
+print(x != y) #True
+print(x < y) #True
+print(x <= y) #True
+print(x > y) #False
+print(x >= y) #False
+print(x is y) #False
+print(x is not y) #True
+
+#3.2.......operadores logicos
+x1 = 6
+y1 = 7
+print(x1 >= 0 and x1 <= 10) #True
+print((x1*2)%2 == 0 or (y1*3)%2 == 0) #True 
