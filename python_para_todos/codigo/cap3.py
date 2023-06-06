efc21a846ffbb7a51bb25c8ec8938900d44c420d
Moises Ayala <mayalam1991@gmail.com>
2023-06-06 16:03:22 -0600

    06/06/2023 16:03 oficina

--- python_para_todos/codigo/cap3.py
+++ python_para_todos/codigo/cap3.py
@@ -1,4 +1,4 @@
-#EJECUCION CONDICIONAL
+#EJECUCION CONDICIONAL pdf pg 45
 #3.1.......expresiones booleanas
 x = 4
 y = 5
@@ -16,3 +16,13 @@ x1 = 6
 y1 = 7
 print(x1 >= 0 and x1 <= 10) #True
 print((x1*2)%2 == 0 or (y1*3)%2 == 0) #True 
+print(not(x1 == y1)) #True
+
+#3.3.......ejecucion condicional
+x2 = 5
+
+if x2 <= 10:
+    print("este numero es menor que 11")
+
+if x2 > 0:
+    pass #esto permite terminar luego
