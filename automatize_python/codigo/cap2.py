a35149e8eb34426e83f786835cc44d40d9e0417c
Moises Ayala <mayalam1991@gmail.com>
2023-06-22 16:07:23 -0600

    22/06/2023 16:07 oficina

--- automatize_python/codigo/cap2.py
+++ automatize_python/codigo/cap2.py
@@ -11,7 +11,42 @@ print(a >= b)
 
 #.......operadores logicos o booleanos binarios
 print(a != b and a <= b) #True
-print()
+print(b < a or b < 10) #True
+print(not(a < b)) #False
+print(not not(a < b)) #True
+
+#.......declaracion if
+name = "moises"
+if name == "moises":
+    print("hola " + name)
+
+#.......declaracion else
+name = input("ingresa tu nombre: \n")
+if name == "Moises":
+    print("Hola " + name + ", tu nombre mide " + str(len(name)) + " letras")
+else: 
+    print("No te conozco, pero tu nombre mide " + str(len(name)) + " letras")
+
+#.......declaracion elif
+nombre = input("Cual es tu nombre: \n")
+print("Hola " + nombre + ", para acceder a nuestro contenido dependera de tu edad")
+edad = input("Introduce ahora tu edad: \n")
+if int(edad) > 0 and int(edad) < 15: 
+    print("Ummm!, lo siento no tengo nada para menores de 15 años")
+elif int(edad) >= 16 and int(edad) <= 30:
+    print("Te mostraremos opciones de carrera y de negocios que pueden interesarte")
+else: 
+    print("Tu pasas de los 30, para ti tenemos un mundo oportunidades")
+
+#......declaracion while
+nombre = input("Debes ingresar con tu usuario: \n")
+while nombre != "Moises":
+    print("Ese no es tu nombre de usuario")
+
+
+
+
+
 
 
 
---
+++ automatize_python/practica.py
@@ -0,0 +1,4 @@
+nombre = ""
+while nombre != "Moises":
+    nombre = input("Debes ingresar con tu usuario: \n")
+    print("Ese no es tu nombre de usuario")
