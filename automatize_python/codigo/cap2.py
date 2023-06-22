da346cf0a74ab67f73d2a3435101218c6788ecc6
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-06-22 07:31:55 -0600

    22/06/2023 07:31 casa

---
+++ automatize_python/codigo/cap2.py
@@ -0,0 +1,17 @@
+#CONTROL DE FLUJO
+#.......operadores de comparacion o relacionales
+a = 1
+b = 2
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+#.......operadores logicos o booleanos binarios
+print(a != b and a <= b) #True
+print()
+
+
+
