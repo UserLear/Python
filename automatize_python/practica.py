14a8ac0e2374b4e4112ce4007a507f74b4b09bf6
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-06-29 07:08:46 -0600

    29/06/2023 07:08 casa

--- automatize_python/practica.py
+++ automatize_python/practica.py
@@ -1,27 +1,52 @@
-"""ingresa = input("Ingresa un numero: ") 
-     
-def collatz(numero):
-    if numero % 2 == 0:
-        print(numero)
-        valor = numero // 2
-        return valor
-    elif numero % 2 == 1:
-        print(numero)
-        valor = 3 * numero + 1
-        return valor
-
-collatz(3)
-while True:
+#ingresa = input("Ingresa un numero: ") 
+"""while True:
     convertir = int(ingresa)
-    collatz(convertir) """
+    def collatz(numero):
+        if numero % 2 == 0:
+            print(numero)
+            valor = numero // 2
+            return valor
+        elif numero % 2 == 1:
+            print(numero)
+            valor = 3 * numero + 1
+            return valor
+    llamada = collatz(convertir)
+    if llamada == 1:
+        break"""
 
-def multiplica(a):
-    return a * 5
+"""while True:
+    introducir = input("Ingresa algo: ")
+    convert = int(introducir)
+    result = 0
+    while result > 1:
+        def collatz(numero):
+            if numero % 2 == 0:
+                print(numero)
+                valor = numero // 2
+                return valor
+            elif numero % 2 == 1:
+                print(numero)
+                valor = 3 * numero + 1
+                return valor
+        result = collatz(convert)"""
 
-resulta = multiplica(3)
+#prueba bucle while con def
+def multiplica(n):
+    return n + 3
 
-suma = resulta * 3
-print(suma)
+valor = input()
+conver = int(valor)
+while True:
+    multiplica(conver)
+
+
+
+
+
+
+
+
+
 
     
     
