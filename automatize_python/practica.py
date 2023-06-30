6f17b437d8168dfcd86d41217628d5c82790e805
Moises Ayala <mayalam1991@gmail.com>
2023-06-30 16:24:32 -0600

    30/06/2023 16:24 oficina

--- automatize_python/practica.py
+++ automatize_python/practica.py
@@ -31,18 +31,55 @@
         result = collatz(convert)"""
 
 #prueba bucle while con def
-def multiplica(n):
-    if n % 2 == 0:
-        return n + 3
-    else:
-        return n + 5
-
+"""def resta(n):
+    return n - 1
 
 valor = input()
-conver = int(valor)
+peticion = int(valor)
 
-while True:
-    print(multiplica(conver))
+while True: 
+    modificado = resta(peticion)
+    guardado = 0
+    while guardado > 1:
+        guardado = modificado
+        rest = resta(guardado)"""
+
+
+def multiplica(n):
+    return n*2
+
+def multiOpcion(n):
+    if n == 2:
+        print("la variable tomo el valor dos")
+    else:
+        print("la variable es cualquier numero menos dos")
+
+#variable introducida y modificada
+valor = input()
+convert = int(valor)
+valor1 = 0
+valor1 = convert
+
+while valor1 != 1:
+    print(valor1)
+    valor1 -= 1 #si no existe el modificador de variable se repite infinitamente
+    multiOpcion(valor1)
+
+#buscar un modificador de variable dentro de while
+def collatz(numero):
+    if numero % 2 == 0:
+        print(numero)
+        valor = numero // 2
+        return valor
+    elif numero % 2 == 1:
+        print(numero)
+        valor = 3 * numero + 1
+        return valor
+
+
+
+
+
     
 
 
