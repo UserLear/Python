0375942367f761a3beaf9443981eda6cb9024187
Moises Ayala <mayalam1991@gmail.com>
2023-07-07 17:10:56 -0600

    07/07/2023 17:10 oficina

--- automatize_python/codigo/cap4.py
+++ automatize_python/codigo/cap4.py
@@ -262,8 +262,79 @@ copiaListaDeListas1 = listaDeListas.copy()
 print(copiaListaDeListas)
 print(copiaListaDeListas1)
 
+#.......juego de la vida
+# Conway's Game of Life
+import random, time, copy
+WIDTH = 60
+HEIGHT = 20
 
+# Create a list of list for the cells:
+nextCells = []
+for x in range(WIDTH):
+    column = [] # Create a new column.
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            column.append('#') # Add a living cell.
+        else:
+            column.append(' ') # Add a dead cell.
+    nextCells.append(column) # nextCells is a list of column lists.
+##########print(nextCells) #crea una lista de listas - (escoge 20 elementos entre '#' o ' ', esto lo ejecuta 60 veces)
 
+while True: # Main program loop.
+    print('\n\n\n\n\n') # Separate each step with newlines.
+    currentCells = copy.deepcopy(nextCells)
+##########crea 5 lineas en blanco para ejecutar el proximo bloque y hacemos una copia
+
+    # Print currentCells on the screen:
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(currentCells[x][y], end='') # Print the # or space.
+        print() # Print a newline at the end of the row.
+##########tenemos 60 listas con 20 elementos cada una, primero: en [x] entra a cada una de las 60 listas este comienza desde el indice 0 y va llamando todas las listas hasta llegar a 60. 
+##########segundo: [y] comienza a llamar cada elemento de cada lista comenzando desde 0, llama a cada elemento 0 de las 60 listas y los une en una cadena.  
+
+    # Calculate the next step's cells based on current step's cells:
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates:
+            # `% WIDTH` ensures leftCoord is always between 0 and WIDTH - 1
+            leftCoord  = (x - 1) % WIDTH
+            rightCoord = (x + 1) % WIDTH
+            aboveCoord = (y - 1) % HEIGHT
+            belowCoord = (y + 1) % HEIGHT
+##########
+
+            # Count number of living neighbors:
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-left neighbor is alive.
+            if currentCells[x][aboveCoord] == '#':
+                numNeighbors += 1 # Top neighbor is alive.
+            if currentCells[rightCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-right neighbor is alive.
+            if currentCells[leftCoord][y] == '#':
+                numNeighbors += 1 # Left neighbor is alive.
+            if currentCells[rightCoord][y] == '#':
+                numNeighbors += 1 # Right neighbor is alive.
+            if currentCells[leftCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-left neighbor is alive.
+            if currentCells[x][belowCoord] == '#':
+                numNeighbors += 1 # Bottom neighbor is alive.
+            if currentCells[rightCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-right neighbor is alive.
+
+            # Set cell based on Conway's Game of Life rules:
+            if currentCells[x][y] == '#' and (numNeighbors == 2 or
+numNeighbors == 3):
+                # Living cells with 2 or 3 neighbors stay alive:
+                nextCells[x][y] = '#'
+            elif currentCells[x][y] == ' ' and numNeighbors == 3:
+                # Dead cells with 3 neighbors become alive:
+                nextCells[x][y] = '#'
+            else:
+                # Everything else dies or stays dead:
+                nextCells[x][y] = ' '
+    time.sleep(1) # Add a 1-second pause to reduce flickering.
 
 
 
