d8007e1c736755aa66b7454f7cff38fc9bd5f4f5
Moises Ayala <mayalam1991@gmail.com>
2023-07-08 13:11:22 -0600

    08/07/2023 13:11 oficina

---
+++ automatize_python/codigo/cap5.py
@@ -0,0 +1,9 @@
+#DICCIONARIOS Y DATOS DE ESTRUCTURACION
+#.......diccionarios
+miGato = {'tamaño': 'gordo', 'color': 'gris', 'disposición': 'fuerte'}
+tamaño = miGato['tamaño'] 
+print('Mi gato esta muy ' + tamaño) #Mi gato esta muy gordo
+
+spam = {12345: 'Combinación de equipaje', 42: 'La respuesta'} #valores enteros por clave
+
+#.......diccionarios poderosos
