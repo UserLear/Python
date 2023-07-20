427405f6790b04bb7ed1816e19db03493ab4d819
Moises Ayala <mayalam1991@gmail.com>
2023-07-20 16:21:32 -0600

    20/07/2023 16:21 oficina

--- automatize_python/practica.py
+++ automatize_python/practica.py
@@ -1,5 +1,33 @@
-import re
-regex1 = re.compile(r'\w+') #imprime caracteres en linea y que sean del mismo tipo
-resul = regex1.search(':{}"#$ 1234 : 3 ] caracter') 
-print(resul)
+while True:
+    print('Enter your age:')
+    age = input()
+    try:
+        age = int(age)
+    except:
+        print('Please use numeric digits.')
+        continue
+    if age < 1:
+        print('Please enter a positive number.')
+        continue
+    break
+
+print(f'Your age is {age}.')
+
+"""When you run this program, the output could look like this:
+
+Enter your age:
+five
+Please use numeric digits.
+Enter your age:
+-2
+Please enter a positive number.
+Enter your age:
+30
+Your age is 30."""
+
+
+
+
+
+
 
