3d9aae315dee5c32f792387bd0d0932a9791acec
Moises Ayala <mayalam1991emanuel@gmail.com>
2023-07-25 06:39:42 -0600

    24/07/2023 21:00 casa

--- automatize_python/codigo/cap7.py
+++ automatize_python/codigo/cap7.py
@@ -238,6 +238,30 @@ robocop.search('ROBOCOP protects the innocent.').group()
 robocop.search('Al, why does your programming book talk about robocop so much?').group()
 #'robocop'
 
+#.......sustitucion de cadenas con el metodo sub()
+namesRegex = re.compile(r'Agent \w+')
+mo = namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.')
+print(mo) #'CENSORED gave the secret documents to CENSORED.'
+
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+mo = agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.')
+print(mo) #A**** told C**** that E**** knew B**** was a double agent.'
+
+#.......gestion de expresiones regulares complejas
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', re.VERBOSE)
+
+#combiando re.ignorecase, re.dotall, re.verbose
+someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL | re.VERBOSE)
+#https://nostarch.com/automatestuff2/
+
+
 
 
 
--- automatize_python/practica.py
+++ automatize_python/practica.py
@@ -1,8 +1,4 @@
-import re
-#.......sustitucion de cadenas con el metodo sub()
-namesRegex = re.compile(r'Agent \w+')
-namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.')
-#'CENSORED gave the secret documents to CENSORED.'
-
+import pyperclip
+print(pyperclip.paste())
 
 
