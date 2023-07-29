353b970b2ee40b9fd23e9fe18f6be515801b15e2
Moises Ayala <mayalam1991@gmail.com>
2023-07-29 12:20:10 -0600

    29/07/23 12:20 oficina

--- automatize_python/codigo/cap8.py
+++ automatize_python/codigo/cap8.py
@@ -1,1 +1,16 @@
+import pyinputplus as pyip
 #VALIDACION DE ENTRADA
+#.......funcion inputNum
+response = pyip.inputNum() 
+#pasada: cinco
+#devuelve: 'cinco' is not a number
+response1 = pyip.inputNum() 
+#pasada: 5 
+#devuelve: 5
+print(type(response1)) #<class 'int'>
+response2 = pyip.inputNum()
+#pasada: 5.1 
+#devuelve: 5.1
+print(type(response2)) #<class 'float'>
+
+
--- automatize_python/practica.py
+++ automatize_python/practica.py
@@ -1,5 +1,14 @@
-import pyperclip
-print(pyperclip.paste())
+while True:
+    valor = input()
+    convertir = int(valor)
+    if convertir == int():
+        print(convertir, type(convertir))
+    elif convertir == float():
+        print(convertir, type(convertir))
+    else:
+        print(f'\"{valor}\" no es un numero')
+
+
 
 
 
