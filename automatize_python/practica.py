a204a34af08271c540a8234dfa7377d4efada801
Moises Ayala <mayalam1991@gmail.com>
2023-08-05 12:52:50 -0600

    05/08/23 12:52 oficina

--- automatize_python/practica.py
+++ automatize_python/practica.py
@@ -1,5 +1,17 @@
 from pathlib import Path, os
+#.......manejo de rutas absolutas y relativas
+Path.cwd() #shell: WindowsPath('C:/Users/Al/AppData/Local/Programs/Python/Python37')
+#verifica si una ruta es absoluta
+Path.cwd().is_absolute() #shell: True
+Path('spam/bacon/eggs').is_absolute() #shell: False
 
+#para convertir una ruta relativa en absoluta
+Path.cwd() / Path('spam/bacon/eggs') #shell: WindowsPath('C:/Users/DELL_USER_#1/Desktop/spam/bacon/eggs')
+
+#devolver una cadena de la ruta absoluta
+os.path.abspath('spam/bacon/eggs') #shell: 'C:\\Users\\DELL_USER_#1\\Desktop\\spam\\bacon\\eggs'
+
+#devolver True si el argumento es ruta absoluta y False si es relativa
 
 
 
