5dc69a784a52929932132f3736bbd912a5d1c6f8
Moises Ayala <mayalam1991@gmail.com>
2023-10-26 16:21:24 -0600

    26-10-23 16:21 oficina

---
+++ ruta_python/Curso_Fundamentos_Python/proyectos.py
@@ -0,0 +1,6 @@
+#concatenar datos a una plantilla: presentacion, 
+#piedra papel o tijera
+#zig zag
+#generacion de un presupuesto
+#calcular el dia de nacimiento de una persona
+#calcular un promedio: notas, ventas
--- ruta_python/prueba.py
+++ ruta_python/prueba.py
@@ -1,3 +1,35 @@
+#nuemero de vidas en un juego
+lives = 3
+print(type(lives)) #<class 'int'>
+
+lives = "3"
+print(type(lives)) #<class 'str'>
+
+#representaciones con numeros
+edad = 32
+presupuesto = 100
+
+#con nivel de precision decimal
+temperatura = 12.12
+print(type(temperatura)) #<class 'float'>
+
+#cambiar el valor de la variable
+lives = 2
+print(lives) #2
+lives = 1
+print(lives) #1
+
+#operaciones dentro de variables
+lives = 25 - 13
+print(lives) #12
+
+#asignacion de un nuevo valor a variable
+lives = lives - 1
+print(lives) #11
+
+#asignacion de un nuevo valor con otro metodo
+lives -= 1
+print(lives) #10
 
 
 
