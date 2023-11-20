1ee5b607061cb377bc74f0d964e1cfb26f20630e
Moises Ayala <mayalam1991@gmail.com>
2023-11-20 16:14:26 -0600

    20-11-23 16:14 oficina

--- ruta_python/Curso_Fundamentos_Python/codigo/21.py
+++ ruta_python/Curso_Fundamentos_Python/codigo/21.py
@@ -11,31 +11,21 @@ while True:
         print("Fue un gusto contribuir a su arendizaje, hasta pronto.")
         break
     elif numero == "1":     
-        print("\n Elige: 1. para aprender del apostrofe, 2. para aprender del caracter de escape. \n")
+        print("""\nElige: 
+1. para aprender del apostrofe, 
+2. para aprender del caracter de escape. \n""")
         opcion = input("Introduce el numero: \n")
         if opcion == '1':
-            print("""El apostrofe se utiliza en el idioma ingles para realizar contracciones utilizando el verbo To Be, \n
-              Por ejemplo: 
-              I am: I'm 
-              You are: You're 
-              He is: He's 
-              She is: She's 
-              You are: You're 
-              We are: We're 
-              They are: They're \n""")
+            print("""\nEl apostrofe se utiliza en el idioma ingles para realizar contracciones utilizando el verbo To Be.
+Para esto debes recordar que existen dos formas que crear cadenas, una es utilizando comillas simple (') otra es utilizando comilla doble (")
+              1. comilla simple (\'): comunmente la utilizamos en el idioma ingles para hacer una contracciones (\"I\'m Moises\").
+              2. commilla doble (\"): comunmente la utilizamos para resaltar un termino entre comillas (\'Le decian el \"Holandez\" \').
+               \n""")
         elif opcion == "2": 
-            def entrada():
-                caracter_escape = input("Introduce siempre la \\ mas el caracter deseado: \n")
-                return  caracter_escape
             print("\n El caracter de escape (\\) se utiliza para introducir caracteres especiales en la cadena.\n")
-            cadena_prueba1 = "Esta cadena es de prueba"
-            cadena_prueba2 = "experimenta con ella \n"
-            print("""1. \\: para introducir una diagonal invertida.
+            print("""1. \\: para introducir una diagonal invertida, introduce doble diagonal invertida en la cadena.
+                    Ejemplo: Hola Marcos \\ Hola Luis.
 2. \n""")
-            seleccion = input("Selecciona el numero del tema para ver como funiona: \n")
-            if seleccion == "1":
-                caracter = entrada()
-                print(cadena_prueba1 + caracter + " " + cadena_prueba2)
 
 
         
