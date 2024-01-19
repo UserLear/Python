9b18b3c3cdc055300efab1c78132c4495d7efe81
Moises Ayala <mayalam1991@gmail.com>
2024-01-19 08:14:14 -0600

    19-01-24 08:10 casa

--- Alura1/p-programacion-6g/proyecto/app.js
+++ Alura1/p-programacion-6g/proyecto/app.js
@@ -1,21 +1,25 @@
 //variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor: ");
+let numeroUsuario;
 
-console.log(numeroUsuario);
+while (numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor: ");
+    console.log(numeroUsuario);
 
-/*Este codigo 
-realiza una comparacion*/
-if(numeroUsuario == numeroSecreto){
-    //devuelve el mensaje alert si a condicion se cumple
-    console.log("Acertaste el numero")
-    alert("Acertaste el numero")
-} else {
-    console.log(`Lo siento, el numero secreto era: ${numeroSecreto} y elegiste el numero: ${numeroUsuario}.`)
-    alert("Lo siento, el numero secreto era: "+ numeroSecreto + " y elegiste el numero:"+ numeroUsuario)
-    //devuelve el mensaje si la condicion no se cumple
+    /*Este codigo 
+    realiza una comparacion*/
+    if(numeroUsuario == numeroSecreto){
+        //devuelve el mensaje alert si a condicion se cumple
+        console.log("Acertaste el numero")
+        alert("Acertaste el numero")
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert("El numero secreto es menor.")
+        } else {
+            alert("El numero secreto es mayor")
+        }
+    //    console.log(`Lo siento, el numero secreto era: ${numeroSecreto} y elegiste el numero: ${numeroUsuario}.`)
+    //    alert("Lo siento, el numero secreto era: "+ numeroSecreto + " y elegiste el numero:"+ numeroUsuario)
+        //devuelve el mensaje si la condicion no se cumple
+    }
 }
-
-
-
-
--- Alura1/p-programacion-6g/proyecto/varios.js
+++ Alura1/p-programacion-6g/proyecto/varios.js
@@ -72,3 +72,31 @@ if (numero < 0) {
     alert("El numero que ingresastes es cero.")
 }
 
+/*3. Crea un sistema de puntuación para un juego. Si la puntuación es 
+mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso 
+contrario, muestra "Intenta nuevamente para ganar.". */
+nombre = prompt("Cual es tu nombre: ")
+
+numero = prompt(`Hola ${nombre} introduce un numero: `)
+
+if (numero >= 100) {
+    alert("Felicidades has ganado.")
+} else {
+    alert("Intenta nuevamente para ganar")
+}
+
+/*4. Crea un mensaje que informe al usuario sobre el saldo de su 
+cuenta, utilizando un template string para incluir el valor del 
+saldo */
+nombre = prompt("Cual es tu nombre: ")
+alert(`Hola ${nombre} este programa te informa que saldo tienes en cuenta`)
+
+let salario = 12000;
+
+alert(`${nombre} al parecer tienes ${salario} disponible para gastar.`)
+
+/*5. Pide al usuario que ingrese su nombre mediante un prompt. 
+Luego, muestra una alerta de bienvenida usando ese nombre.*/
+alert("Hola usuario ingresa por favor tu nombre");
+nombre = prompt("Dinos tu nombre: ");
+alert(`Valla te estabamos esperando ${nombre}.`);
