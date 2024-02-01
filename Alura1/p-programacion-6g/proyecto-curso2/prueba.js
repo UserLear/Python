7df322b59c6d4e5b86d4a54f4bc1b112fb5cd6de
Moises Ayala <mayalam1991@gmail.com>
2024-02-01 16:40:42 -0600

    01-02-24 16:40 oficina

--- Alura1/p-programacion-6g/proyecto-curso2/prueba.js
+++ Alura1/p-programacion-6g/proyecto-curso2/prueba.js
@@ -1,10 +1,29 @@
-compara1 = (`\`` === "`");
-console.log(`\``);
-console.log("`");
-console.log(compara1);
+//DESAFIOS
+/*1. Crea una función que calcule el índice de masa 
+corporal (IMC) de una persona a partir de su altura en 
+metros y peso en kilogramos, que se recibirán como 
+parámetros.*/
+function indiceMasaCorporal (peso, altura) {
+    let iMc = peso / (altura**2)
+    return iMc
+}
+console.log(indiceMasaCorporal(180, 1.78))
 
-compara2 = (`\${1}` === "${1}");
-console.log(`\${1}`);
-console.log("${1}");
-console.log(compara2)
+/*2.  Crea una función que calcule el valor del factorial 
+de un número pasado como parámetro.*/
+function factorial(numero) {
+    let valor = parseInt(numero);
+    let factorial = 1;
+    while (true) {
+        if (valor > 0) {
+            factorial *= valor;
+            valor--;
+        } else {
+            break
+        }
+    }
+    return factorial
+}
+let valor1 = factorial(10)
+console.log(valor1)
 
